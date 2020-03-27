4c25ebd67a082d25578201c2639fb67d3a5099eb
Nevin Liber <nliber@anl.gov>
2020-03-27 02:16:28 +0000

    Changed namespace

--- CopyHostToHost.cpp
+++ CopyHostToHost.cpp
@@ -1,25 +1,32 @@
 #include <Kokkos_Core.hpp>
 #include <iostream>
 
-template <typename V>
-void printView(V const& v, std::ostream& os = std::cout) {
-    auto const size = v.size();
-    for (size_t s = 0; s != size; ++s) os << v[s] << '\n';
+template<typename... Ts>
+void printView(Kokkos::View<int*, Ts...> const& v, std::ostream& os = std::cout) {
+    os << __PRETTY_FUNCTION__ << '\n';
+    Kokkos::View<int*, Kokkos::Experimental::SYCLHostUSMSpace> vv("VV", v.size());
+    Kokkos::deep_copy(vv, v);
+    for (size_t s = 0; s != vv.size(); ++s) os << vv[s] << '\n';
     os << '\n';
 }
 
 int main() {
     //using Space = Kokkos::HostSpace;
-    using Space = Kokkos::Experimental::HostUSMMemorySpace;
+    using Space = Kokkos::Experimental::SYCLHostUSMSpace;
+    //using Space = Kokkos::Experimental::SYCLDeviceUSMSpace;
 
     Kokkos::ScopeGuard _;
 
     Kokkos::View<int*, Space> v("V", 10);
     printView(v);
-
+#if 0
     Kokkos::parallel_for(
         10, KOKKOS_LAMBDA(const size_t i)
         { v[i] = i; });
+#else
+    for (size_t s = 0; s != v.size(); ++s)
+        v[s] = s;
+#endif
     printView(v);
 
     Kokkos::View<int*, Space> w("W", v.size());
