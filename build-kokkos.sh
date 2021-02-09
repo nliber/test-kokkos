d4735ec898d5f9fad7058dcd9696f5fe7b0359f2
Nevin Liber <nliber@anl.gov>
2021-02-09 23:45:11 +0000

    Generalized the BLD directory to use build type

--- build-kokkos.sh
+++ build-kokkos.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -xv
+
+module purge
 module load oneapi
 
+export CMAKE_BUILD_TYPE="debug"
 export KOKKOS_DIR="${PWD}"
-export BLD="${KOKKOS_DIR}"/build_icpx
+export BLD="${KOKKOS_DIR}"/build
 export INSTALL="${BLD}"/install
 rm -r "${BLD}"
 mkdir -p "${INSTALL}"
 cd "${BLD}"
 
-cmake ..\
+cmake \
     -DCMAKE_CXX_COMPILER="$(type -P icpx)" \
-    -DCMAKE_CXX_FLAGS="-ggdb3 -DCL_TARGET_OPENCL_VERSION=210" \
+    -DCMAKE_CXX_FLAGS="-ggdb3" \
     -DCMAKE_EXE_LINKER_FLAGS= \
     -DCMAKE_INSTALL_PREFIX="${INSTALL}" \
     -DKokkos_ENABLE_SYCL=ON \
@@ -20,9 +23,9 @@ cmake ..\
     -DKokkos_CXX_STANDARD=17 \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -DCMAKE_CXX_EXTENSIONS=OFF \
-    -DCMAKE_BUILD_TYPE=DEBUG \
+    -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
     -DKokkos_ENABLE_DEBUG=ON \
-    ..
+    ~-
 
 make install -j
 
