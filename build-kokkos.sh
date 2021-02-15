ea3ec12016b3f179833d32a5a19b5c0eef79d53d
Nevin Liber <nliber@anl.gov>
2021-02-15 18:48:14 +0000

    Cleaned up and generalized an number of the shell variables so that we can have different builds of the same source under the same directory.

--- build-kokkos.sh
+++ build-kokkos.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-set -xv
 
 module purge
 module load oneapi
 
-export CMAKE_BUILD_TYPE="debug"
-export KOKKOS_DIR="${PWD}"
-export BLD="${KOKKOS_DIR}"/build
-export INSTALL="${BLD}"/install
-rm -r "${BLD}"
-mkdir -p "${INSTALL}"
-cd "${BLD}"
+declare oneapi_version="$(module info-loaded oneapi)"
+declare oneapi_version="${oneapi_version#oneapi/}"
+declare cmake_build_type=debug
+declare kokkos_dir="${PWD}"
+declare bld="${kokkos_dir}"/build/"${oneapi_version}"/"${cmake_build_type}"
+declare install="${bld}"/install
+declare cmake_cxx_compiler="$(type -P icpx)"
 
+rm -rf "${bld}" "${install}"
+mkdir -p "${bld}" "${install}"
+
+cd "${bld}"
 cmake \
-    -DCMAKE_CXX_COMPILER="$(type -P icpx)" \
+    -DCMAKE_CXX_COMPILER="${cmake_cxx_compiler}" \
     -DCMAKE_CXX_FLAGS="-ggdb3" \
     -DCMAKE_EXE_LINKER_FLAGS= \
-    -DCMAKE_INSTALL_PREFIX="${INSTALL}" \
+    -DCMAKE_INSTALL_PREFIX="${install}" \
     -DKokkos_ENABLE_SYCL=ON \
     -DKokkos_ENABLE_TESTS=ON \
     -DKokkos_ENABLE_EXAMPLES=OFF \
     -DKokkos_CXX_STANDARD=17 \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DCMAKE_VERBOSE_MAKEFILE=OFF \
     -DCMAKE_CXX_EXTENSIONS=OFF \
-    -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+    -DCMAKE_BUILD_TYPE="${cmake_build_type}" \
     -DKokkos_ENABLE_DEBUG=ON \
     ~-
 
