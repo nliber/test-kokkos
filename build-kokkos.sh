91f64acab1458ec0c435191dae6a39696d0f313f
Nevin Liber <nliber@anl.gov>
2021-03-02 18:10:23 +0000

    Added compdb output for build

--- build-kokkos.sh
+++ build-kokkos.sh
@@ -4,13 +4,15 @@ module purge
 #module load oneapi/2020.12.15.005
 module load oneapi
 
+declare pwd="${PWD}"
 declare oneapi_version="$(module info-loaded oneapi)"
 declare oneapi_version="${oneapi_version#oneapi/}"
 declare cmake_build_type=debug
-declare kokkos_dir="${PWD}"
+declare kokkos_dir="${pwd}"
 declare bld="${kokkos_dir}"/build/"${oneapi_version}"/"${cmake_build_type}"
 declare install="${bld}"/install
 declare cmake_cxx_compiler="$(type -P icpx)"
+declare compdb="compile_commands.json"
 
 rm -rf "${bld}" "${install}"
 mkdir -p "${bld}" "${install}"
@@ -22,7 +24,7 @@ cmake \
     -DCMAKE_EXE_LINKER_FLAGS= \
     -DCMAKE_INSTALL_PREFIX="${install}" \
     -DKokkos_ENABLE_SYCL=ON \
-    -DKokkos_ENABLE_TESTS=OFF \
+    -DKokkos_ENABLE_TESTS=ON \
     -DKokkos_ENABLE_EXAMPLES=OFF \
     -DKokkos_CXX_STANDARD=17 \
     -DCMAKE_VERBOSE_MAKEFILE=OFF \
@@ -30,9 +32,10 @@ cmake \
     -DCMAKE_BUILD_TYPE="${cmake_build_type}" \
     -DKokkos_ENABLE_DEBUG=ON \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -GNinja \
-    ~-
+    "${kokkos_dir}"
 
-#make -j install
-ninja install
+#cmake --build . --target install
+make -j install
+
+[[ -r "${compdb}" ]] && ln -fsv "${compdb}" "${kokkos_dir}"
 
